379c3a0fd228d38970d0374cfc7a96a4d3ad0363
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2024-09-30 20:32:48 -0300

    :boom: fix: revertendo mudanças ineficientes

--- src/projeto/banco/model/conta/IConta.java
+++ src/projeto/banco/model/conta/IConta.java
@@ -6,33 +6,33 @@ import java.util.List;
 import projeto.banco.model.transacao.RegistroTransacao;
 
 public interface IConta {
-	 Integer getNumero();
+	Integer getNumero();
 
-	 void setNumero(int numero);
+	void setNumero(int numero);
 
-	 String getTipo();
+	String getTipo();
 
-	 void setTipo(String tipo);
+	void setTipo(String tipo);
 
-	 BigDecimal getSaldo();
+	BigDecimal getSaldo();
 
-	 void setSaldo(BigDecimal saldo);
+	void setSaldo(BigDecimal saldo);
 
-	 String getCpfTitular();
+	String getCpfTitular();
 
-	 void setCpfTitular(String cpf);
+	void setCpfTitular(String cpf);
 
-	 Boolean getStatus();
+	Boolean getStatus();
 
-	 void setStatus(Boolean status);
+	void setStatus(Boolean status);
 
-	 void setDetalhesConta(Integer numero, String tipo, BigDecimal saldo, String cpfTitular, Boolean status);
+	void setDetalhesConta(Integer numero, String tipo, BigDecimal saldo, String cpfTitular, Boolean status);
 
-	 Boolean depositar(BigDecimal quantia);
+	Boolean depositar(BigDecimal quantia);
 
-	 Boolean sacar(BigDecimal quantia);
+	Boolean sacar(BigDecimal quantia);
 
-	 Boolean transferir(BigDecimal quantia, int contaDestino);
+	Boolean transferir(BigDecimal quantia, int contaDestino);
 
-	 List<RegistroTransacao> emitirExtrato(int numeroConta, int mes, int ano);
+	List<RegistroTransacao> emitirExtrato(int numeroConta, int mes, int ano);
 }
